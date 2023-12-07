ee2a9198fc17509824f85832b02788ba9aa5ce79
benjamin <benjamin.ostendorf@gmx.de>
2023-12-08 00:32:55 +0100

    snapshot-commit

--- trello_ticket_mail_manager/deploy.sh
+++ trello_ticket_mail_manager/deploy.sh
@@ -5,5 +5,5 @@ DEST=host224.checkdomain.de
 DIR=ticket-mail-management.trello-powerup.hchs.hamburg
 echo "please make sure to enable key based authentication for $U@$DEST"
 
-tsc
+npm run build
 rsync -ap dist/* $U@$DEST:$DIR/.
--- trello_ticket_mail_manager/src/main.ts
+++ trello_ticket_mail_manager/src/main.ts
@@ -1,19 +1,17 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-
+import { test, stringify } from 'typescript_helpers';
 
 window.TrelloPowerUp.initialize({
   "card-badges": async function (t: any, options: any) {
     console.log("called card-badges function");
     let cardId = await t.card('id').get('id');
-//    console.log(stringify("test"));
-//    alert(stringify("called some function"));
+      console.log(stringify("test"));
     return [
       {
-        text: "test badge",
+        text: cardId,
         color: "red"
       }
     ]
   }
 })
+
