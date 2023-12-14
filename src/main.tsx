b0c6d0e544122472eb61be42704334656c751afe
benjamin <benjamin.ostendorf@gmx.de>
2023-12-14 23:40:03 +0100

    add routes for card_button and board_button -> subject to change import AddListenerMail component to display as card button iframe initialize already implemented trello capabilities

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import './index.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { IndexPage } from './IndexPage'
 import { initBadges } from './trello_capabilities/card_badges';
+import { initBoardButtons } from './trello_capabilities/board_buttons';
+import { initCardButtons } from './trello_capabilities/card_buttons';
+import AddListenerMail from './trello_capabilities/iframes/card_buttons/AddListenerMail';
 
 declare global {
   interface Window {
@@ -15,14 +18,17 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <Routes>
       <Route path="/" element={<IndexPage />}>
       </Route>
-      <Route path='board_button' element={<div>board_button</div>}>
+      <Route path='/board_button' element={<div>board_button</div>}>
       </Route>
+      <Route path='/card_button' element={<AddListenerMail />} />
     </Routes>
   </BrowserRouter>
 )
 
 window.TrelloPowerUp.initialize({
-  "card-badges": initBadges
+  "card-badges": initBadges,
+  "board_buttons": initBoardButtons,
+  "card-buttons": initCardButtons,
 })
 
 
