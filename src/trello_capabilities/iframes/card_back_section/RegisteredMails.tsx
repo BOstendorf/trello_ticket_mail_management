1b92b3e7bf29f893517f415137ef7a87c75240b9
benjamin <benjamin.ostendorf@gmx.de>
2023-12-19 00:05:26 +0100

    add font awesome icons

--- src/trello_capabilities/iframes/card_back_section/RegisteredMails.tsx
+++ src/trello_capabilities/iframes/card_back_section/RegisteredMails.tsx
@@ -1,8 +1,15 @@
+import { AddMailForm } from "../ui_elements/AddEmailForm";
+import MailList from "../ui_elements/MailList";
 
 
 export default function RegisteredMails() {
 
-  return <div>
-    registeredMails Section
+  const confirmCallback = async (mail: string) => {
+
+  }
+
+  return <div className={"p-2 rounded-lg bg-white"}>
+    <AddMailForm confirmCallback={confirmCallback} />
+    <MailList mails={[]}/>
   </div>
 }
