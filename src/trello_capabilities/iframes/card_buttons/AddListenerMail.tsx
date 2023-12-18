b87e18d11a39a19bbffdb79fccb70bb491323374
benjamin <benjamin.ostendorf@gmx.de>
2023-12-18 22:49:17 +0100

    extract form logic and use newly defined component

--- src/trello_capabilities/iframes/card_buttons/AddListenerMail.tsx
+++ src/trello_capabilities/iframes/card_buttons/AddListenerMail.tsx
@@ -1,19 +1,10 @@
-import { useState } from "react"
-import { TrelloInterface } from "../../shared_definitions";
 import { isList, stringify } from "typescript_helpers";
-import { is } from "typia";
 import { mailIsValid } from "typescript_helpers/out/src/helper_functions";
 import { TrelloSingleKeySet } from "../../definitions/TrelloSet";
+import { AddMailForm } from "../ui_elements/AddEmailForm";
 export default function AddListenerMail() {
-  const [mail, setMail]: [string, (value: string) => void] = useState("");
-  const [mailValid, setMailValid]: [boolean, (value: boolean) => void] = useState(false);
 
-  const onMailChange = (value: string) => {
-    setMail(value);
-    setMailValid(mailIsValid(value));
-  }
-
-  const buttonCallback = async () => {
+  const confirmCallback = async (mail: string) => {
     if (!mailIsValid(mail)) {
       alert(`mail ${mail} is not valid mail address`);
       return;
@@ -33,18 +24,6 @@ export default function AddListenerMail() {
   }
 
   return <div className={"bg-white p-2 h-32 rounded-lg"}>
-      <form>
-    <label className={"inline mb-2 text-sm font-medium text-gray-900"}>
-      Email Adresse
-    </label>
-    <input type="email" id="email" className={`bg-gray-200 border text-gray-900 text-sm rounded-lg block w-full p-1 ${!mailValid ? 'border-red-400 hover:border-red-500 focus:border-red-500' : 'border-gray-200 hover:border-gray-400 focus:ring-blue-500 focus:border-blue-500'}`} 
-      onChange={(event) => {onMailChange(event.target.value)}}
-      placeholder="Mail"/>
-      <button 
-        className={`text-white hover:bg-blue-700 font-medium rounded-lg text-sm text-center px-5 p-1 mt-2 ${!mailValid ? 'bg-gray-200' : 'bg-blue-500'}`}
-        onClick={()=>{buttonCallback()}}>
-        Add Mail
-      </button>
-</form>
+    <AddMailForm confirmCallback={confirmCallback}/>
   </div>
 }
