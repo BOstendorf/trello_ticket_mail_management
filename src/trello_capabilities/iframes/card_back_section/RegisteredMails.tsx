2a0581cfb178d7303ddb0d3f7db6d2440b0a13b6
benjamin <benjamin.ostendorf@gmx.de>
2023-12-19 12:09:45 +0100

    add handling for stored mail addresses

--- src/trello_capabilities/iframes/card_back_section/RegisteredMails.tsx
+++ src/trello_capabilities/iframes/card_back_section/RegisteredMails.tsx
@@ -1,16 +1,43 @@
+import { useEffect, useState } from "react";
+import { TrelloInterface } from "../../shared_definitions";
 import { AddMailForm } from "../ui_elements/AddEmailForm";
-import MailList from "../ui_elements/MailList";
+import MailList, { mailEntry } from "../ui_elements/MailList";
+import { is } from "typia";
 
 export const RegisteredMailsSectionHeight = 320;
 
 export default function RegisteredMails() {
+  const [mails, setMails]: [mailEntry[], (value: any) => any] = useState([]);
+  var t: TrelloInterface | null = null;
+
+  const fetchAndSetRegisteredMails = async (t: TrelloInterface) => {
+    let cardId = await t.card('id').get('id');
+    let registeredMails = await t.get(cardId, "shared", "registeredMails");
+    setMails(registeredMails.map((mail: string): mailEntry => {
+      return {mail: mail, deleteCallback: ()=>{}};
+    } ))
+  }
+
+  useEffect(()=>{
+    t = window.TrelloPowerUp.iframe();
+  }, []);
+  
+  useEffect(()=>{
+   
+    /*if (is<TrelloInterface>(t)) {
+      fetchAndSetRegisteredMails(t);
+    }*/
+  }, [t]);
 
   const confirmCallback = async (mail: string) => {
-
+    
+    var t = window.TrelloPowerUp.iframe();
+    let cardId = await t.card('id').get('id');
+    let registeredMails = await t.get(cardId, "shared", "registeredMails");
   }
 
   return <div className={"p-2 rounded-lg bg-white h-80"}>
     <AddMailForm confirmCallback={confirmCallback} />
-    <MailList mails={[]}/>
+    <MailList mails={mails}/>
   </div>
 }
