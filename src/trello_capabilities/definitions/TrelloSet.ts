4f32e3d60e04aabeb3cd85d7c935cc6839e0f145
benjamin <benjamin.ostendorf@gmx.de>
2023-12-19 15:20:47 +0100

    change return type to any. actually returns some sort of promise but the trello documentation isn't clear on what the promise intails and i have currently no idea how to specify a general promise

--- src/trello_capabilities/definitions/TrelloSet.ts
+++ src/trello_capabilities/definitions/TrelloSet.ts
@@ -2,7 +2,7 @@ import { TrelloAPI_scope, TrelloAPI_visibility } from "./TrelloAPI";
 import { Serializable } from "./base_definitions";
 
 
-export type TrelloSingleKeySet = (scope: TrelloAPI_scope, visibility: TrelloAPI_visibility, key: string, value: Serializable) => void;
-export type TrelloMultiKeySet = (scope: TrelloAPI_scope, visibility: TrelloAPI_visibility, values: {[key: string]: Serializable}) => void;
+export type TrelloSingleKeySet = (scope: TrelloAPI_scope, visibility: TrelloAPI_visibility, key: string, value: Serializable) => any;
+export type TrelloMultiKeySet = (scope: TrelloAPI_scope, visibility: TrelloAPI_visibility, values: {[key: string]: Serializable}) => any;
 
 export type TrelloSetFunction = TrelloSingleKeySet | TrelloMultiKeySet;
