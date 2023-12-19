4485c9b716e67f5f9ab9002f11d776c7db054214
benjamin <benjamin.ostendorf@gmx.de>
2023-12-19 10:38:18 +0100

    add function initCardBackSection used to initialize trello capabilities

---
+++ src/trello_capabilities/card_back.ts
@@ -0,0 +1,21 @@
+import { TrelloCardBackSection } from "./definitions/TrelloCardBackSection";
+import { RegisteredMailsSectionHeight } from "./iframes/card_back_section/RegisteredMails";
+import { sampleIcon } from "./sampleIcon";
+import { TrelloInterface, TrelloInterfaceOptions } from "./shared_definitions";
+
+
+export function initCardBackSection(t: TrelloInterface, options: TrelloInterfaceOptions): TrelloCardBackSection {
+  return {
+    title: "Registered Mails",
+    icon: sampleIcon.dark,
+    content: {
+      type: "iframe",
+      url: './card_back_section',
+      height: RegisteredMailsSectionHeight,
+    },
+    action: {
+      text: "test action",
+      callback: (t: TrelloInterface) => {console.log("action callback")},
+    }
+  };
+}
